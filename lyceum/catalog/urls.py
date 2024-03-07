fa9a8e23b0aba480bebad58d566133463c84ab16
Artur Tokarev <artur_s_t_d@icloud.com>
2024-03-08 00:27:59 +0300

    asa

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, re_path
 from . import views
 urlpatterns = [
     path('', views.item_list),
-    path('<int:pk>/', views.item_detail)
+    path('<int:pk>/', views.item_detail),
     re_path(r'^re/(?P<a>[1-9][0-9]*)/', views.item)
 ]
--- lyceum/homepage/urls.py
+++ lyceum/homepage/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+
 from . import views
-urlpatterns = [    path('coffee/', views.cof),
+urlpatterns = [
+    path('coffee/', views.cof),
     path('', views.home)]
