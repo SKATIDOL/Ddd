2bd1f97feff21c03ecbf100a1d4c096341a9d1d5
Artur Tokarev <artur_s_t_d@icloud.com>
2024-03-08 00:19:49 +0300

    asa

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 urlpatterns = [
     path('', views.item_list),
     path('<int:pk>/', views.item_detail)
+    re_path(r'^re/(?P<a>[1-9][0-9]*)/', views.item)
 ]
--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -7,3 +7,7 @@ class StaticURLTests(TestCase):
     def test_homepage_endpoint(self):
         response = Client().get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        def test_homepage_coffee(self):
+            response = Client().get('/coffee/')
+            self.assertEqual(response.status_code, 418)
